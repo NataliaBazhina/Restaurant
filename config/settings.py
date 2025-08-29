a49216237640087e512721df1534ada8945fe6a4
Natalia Bazhina <nataliaagapova27@yandex.ru>
2025-08-30 02:09:39 +0300

    delete double services.py and fix settings celery beat

--- config/settings.py
+++ config/settings.py
@@ -133,10 +133,9 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 
-CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_BEAT_SCHEDULE = {
     "send_reservation_reminders": {
         "task": "reservation.tasks.send_reservation_reminders",
-        "schedule": crontab(minute='*'),
+        "schedule": crontab(hour=8, minute=0),
     },
 }
